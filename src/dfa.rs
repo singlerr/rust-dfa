22dce38598bbcd0badc55c3a10c5c472f3493a6c
singlerr <singlerr@naver.com>
2024-10-22 16:12:08 +0900

    todo: how to define dfa elegantly?

--- src/dfa.rs
+++ src/dfa.rs
@@ -1,8 +1,1 @@
-extern crate proc_macro;
 
-use quote::ToTokens;
-
-struct LanguageSet {
-    characters: Vec<char>,
-}
-struct DFA {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,75 @@
-use quote::ToTokens;
+use std::borrow::Borrow;
+use std::collections::HashMap;
+use std::hash::Hash;
 
 mod dfa;
 
-/// How to define marco?
-///
-#[proc_macro]
-pub fn define(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input = proc_macro2::TokenStream::from(input);
+#[macro_export]
+macro_rules! states {
+    (let ) => {};
+}
 
-    for token in input {
-        match token {
-            (any) => {
-                println!("{:?}", any);
+pub enum Result<S> {
+    Ok(S),
+    End(S),
+    Err,
+}
+
+trait Comp: Eq + Hash {}
+
+pub struct State<S, I> {
+    state_transitions: HashMap<I, S>,
+}
+
+pub struct DFA<S, I> {
+    languages: Box<[I]>,
+    states: HashMap<S, State<S, I>>,
+    final_states: Box<[S]>,
+    state: S,
+}
+
+impl<S, I> DFA<S, I>
+where
+    S: Hash + Eq,
+    I: Hash + Eq,
+{
+    fn is_final(&self, s: &S) -> bool {
+        for final_state in (*self.final_states).iter() {
+            if final_state == s {
+                return true;
             }
         }
+        false
     }
 
-    "println!(\"\")".parse().unwrap()
+    pub fn feed<Q: ?Sized>(&mut self, i: &Q) -> Result<&S>
+    where
+        I: Borrow<Q>,
+        Q: Hash + Eq,
+    {
+        let s = self.states.get(&self.state);
+        match s {
+            Some(s) => {
+                if let Some(s) = s.state_transitions.get(i) {
+                    if self.is_final(s) {
+                        Result::End(s)
+                    } else {
+                        Result::Ok(s)
+                    }
+                } else {
+                    Result::Err
+                }
+            }
+            None => Result::Err,
+        }
+    }
+}
+
+fn a() {
+    let dfa: DFA<u8, char> = DFA {
+        languages: Box::new(['a'; 10]),
+        states: HashMap::new(),
+        final_states: Box::new([10]),
+        state: 0,
+    };
 }
